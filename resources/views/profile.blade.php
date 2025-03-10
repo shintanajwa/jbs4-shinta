063d7ae87cd5559e87b0854ff29f9bda44541b7b
shintanajwa <shintaainun37@gmail.com>
2025-03-10 12:37:15 +0700

    Pesan Commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container mt-4">
+            <div class="card">
+                <div class="card-header">
+                     <h1>Profil Saya</h1>
+                </div>
+                <div class="card-body">
+                    <img src="nayyyo.jpg" alt="Profile Picture" width="150">
+                     <h3>Shinta Ainun Najwa</h3>
+                     <p>Email : shintaainun37@gmail.com</p>
+                     <p>Bio: Siswa SMKN 1 Sayung</p>
+                     <h5>Skills: </h5>
+                     <ul>
+                        <li>Manajemen Waktu</li>
+                        <li>Kerja Tim</li>
+                        <li>Pengelolaan Media Sosial</li>
+                </div>
+            </div>
+         </div>
+    <body>
+</html>
